d543e66b783d1defab5a4d6b2b68aeee00dad977
andou1977 <andouemmanuel@gmail.com>
2025-08-14 04:26:35 +0100

    new commit

--- src/main/java/org/example/Tablelaunch.java
+++ src/main/java/org/example/Tablelaunch.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import app.getxray.xray.junit.customjunitxml.annotations.XrayTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Tag;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -18,10 +20,13 @@ public class Tablelaunch {
         webDriver.get("file:///C:/Users/GENIUS/Downloads/e-commerce-pricing/index.html");
     }
 
+    @XrayTest(key = "TEST-123")
+
+
 
     @Test
     public void launchtable(){
-        // MYF-14 : Vérifier que la connexion fonctionne
+
         table.launchtable();
     }
 }
