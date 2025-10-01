a1d8305ac24bc6551342fc2c9c95dd6200cfdd33
andou1977 <andouemmanuel@gmail.com>
2025-10-01 21:44:12 +0200

    firstcommit

--- src/test/java/org/example/VerifytableauTest.java
+++ src/test/java/org/example/VerifytableauTest.java
@@ -1,31 +1,31 @@
-package org.example;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class VerifytableauTest {
-
-    WebDriver webDriver;
-
-    Verifytableau verifytableau;
-
-
-    @BeforeEach
-    void setup(){
-        WebDriverManager.firefoxdriver().setup();
-        webDriver=new FirefoxDriver( );
-        verifytableau=new Verifytableau(webDriver);
-        webDriver.get("http://localhost:4200/uikit/table");
-
-    }
-
-    @Test
-    void search(){
-        verifytableau.search();
-    }
-}
+//package org.example;
+//
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class VerifytableauTest {
+//
+//    WebDriver webDriver;
+//
+//    Verifytableau verifytableau;
+//
+//
+//    @BeforeEach
+//    void setup(){
+//        WebDriverManager.firefoxdriver().setup();
+//        webDriver=new FirefoxDriver( );
+//        verifytableau=new Verifytableau(webDriver);
+//        webDriver.get("http://localhost:4200/uikit/table");
+//
+//    }
+//
+//    @Test
+//    void search(){
+//        verifytableau.search();
+//    }
+//}
