31b8b081ef538f975438a011eafece190f4705f3
GENIUS <andouemmanuel@gmail.com>
2025-10-23 15:49:12 -0700

    first commit

--- src/test/java/org/example/ReadAndVerifyTest.java
+++ src/test/java/org/example/ReadAndVerifyTest.java
@@ -1,34 +1,34 @@
-package org.example;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ReadAndVerifyTest {
-
-
-    WebDriver webDriver;
-
-    ReadAndVerify readAndVerify;
-
-
-    @BeforeEach
-    public void setup(){
-        WebDriverManager.firefoxdriver().setup();
-        webDriver=new FirefoxDriver();
-        readAndVerify=new ReadAndVerify(webDriver);
-        webDriver.get("https://demoqa.com/upload-download");
-    }
-
-    @Test
-    public void readandverify(){
-        readAndVerify.downloadnow();
-    }
-
-
-}
+//package org.example;
+//
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.junit.Before;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class ReadAndVerifyTest {
+//
+//
+//    WebDriver webDriver;
+//
+//    ReadAndVerify readAndVerify;
+//
+//
+//    @BeforeEach
+//    public void setup(){
+//        WebDriverManager.firefoxdriver().setup();
+//        webDriver=new FirefoxDriver();
+//        readAndVerify=new ReadAndVerify(webDriver);
+//        webDriver.get("https://demoqa.com/upload-download");
+//    }
+//
+//    @Test
+//    public void readandverify(){
+//        readAndVerify.downloadnow();
+//    }
+//
+//
+//}
