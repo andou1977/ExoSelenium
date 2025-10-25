15075af5f84f4d7b8788580f114e096a53f0a1f6
GENIUS <andouemmanuel@gmail.com>
2025-10-25 14:36:48 +0200

    first commit

--- src/main/java/org/example/Verifycontennu.java
+++ src/main/java/org/example/Verifycontennu.java
@@ -9,7 +9,7 @@ public class Verifycontennu {
 
     WebDriver webDriver;
 
-    @FindBy(xpath = "//a[@href='download/test.txt']")
+    @FindBy(xpath = "//a[@href='download/bb.txt']")
     private WebElement  downloadfile;
 
     public Verifycontennu(WebDriver webDriver) {
