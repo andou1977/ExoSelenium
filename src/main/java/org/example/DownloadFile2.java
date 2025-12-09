67f4c068b33c93b8a29a6c2ab0611b1ed573496f
andou1977 <andouemmanuel@gmail.com>
2025-12-09 13:41:30 +0100

    first

--- src/main/java/org/example/DownloadFile2.java
+++ src/main/java/org/example/DownloadFile2.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-//il se lance automatiquement sur jenkins
+
 public class DownloadFile2 {
 
 
